fe2a8ec7b2b8349a36fb4e009347c99b58dd73ae
Bhagia Sheri <38161609+BhagiaSheri@users.noreply.github.com>
2020-08-05 23:21:12 +0500

    Blockchain Based E-Voting System Database

--- blockchain_e_vote.sql
+++ blockchain_e_vote.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.8.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 05, 2020 at 07:32 PM
--- Server version: 10.1.40-MariaDB
--- PHP Version: 7.3.5
+-- Generation Time: Aug 05, 2020 at 08:19 PM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -38,6 +38,23 @@ CREATE TABLE `admin` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `candidates`
+--
+
+CREATE TABLE `candidates` (
+  `c_id` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `email` varchar(35) NOT NULL,
+  `contact_no` varchar(13) NOT NULL,
+  `age` int(11) NOT NULL,
+  `designation` varchar(30) NOT NULL,
+  `no_of_votes` int(11) NOT NULL DEFAULT '0',
+  `profile` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -45,12 +62,38 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `name` varchar(45) NOT NULL,
   `email` varchar(35) NOT NULL,
-  `contact_no` varchar(15) NOT NULL,
+  `contact_no` varchar(13) NOT NULL,
   `age` int(10) NOT NULL,
-  `password` varchar(100) NOT NULL,
+  `password` varchar(30) NOT NULL,
   `profile` blob NOT NULL,
-  `is_active` tinyint(1) NOT NULL DEFAULT '0',
-  `is_vote_casted` tinyint(1) NOT NULL DEFAULT '0'
+  `is_active` tinyint(5) NOT NULL DEFAULT '0',
+  `is_vote_casted` tinyint(5) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vote_timing`
+--
+
+CREATE TABLE `vote_timing` (
+  `t_id` int(11) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `start_time` time NOT NULL,
+  `end_time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `voting_status`
+--
+
+CREATE TABLE `voting_status` (
+  `v_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `candidate_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -64,10 +107,30 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `candidates`
+--
+ALTER TABLE `candidates`
+  ADD PRIMARY KEY (`c_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `vote_timing`
+--
+ALTER TABLE `vote_timing`
+  ADD PRIMARY KEY (`t_id`);
+
+--
+-- Indexes for table `voting_status`
+--
+ALTER TABLE `voting_status`
+  ADD PRIMARY KEY (`v_id`),
+  ADD KEY `user_vote_candidate_fk` (`user_id`),
+  ADD KEY `candidate_vote_user_fk` (`candidate_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -80,10 +143,39 @@ ALTER TABLE `admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `candidates`
+--
+ALTER TABLE `candidates`
+  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `vote_timing`
+--
+ALTER TABLE `vote_timing`
+  MODIFY `t_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `voting_status`
+--
+ALTER TABLE `voting_status`
+  MODIFY `v_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `voting_status`
+--
+ALTER TABLE `voting_status`
+  ADD CONSTRAINT `candidate_vote_user_fk` FOREIGN KEY (`candidate_id`) REFERENCES `candidates` (`c_id`),
+  ADD CONSTRAINT `user_vote_candidate_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
