465492b508aa7f0ed218866c91949d845e258c79
Pooja Kumari <35136890+poojakumari11228@users.noreply.github.com>
2020-08-11 21:32:39 +0500

    login & session updates

--- ajax/controller.js
+++ ajax/controller.js
@@ -1,6 +1,5 @@
 function log_out(){
-    // var log_me_out="true";
-    $.post("php/log_out.php",
+    $.post("../log_out.php",
     function(data){
       // alert("returned: "+data);
       //to redirect back to main page after logging out
