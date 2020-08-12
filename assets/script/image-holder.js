9fda2e653a5311e8e2e51822db9062b70078d900
Pooja Kumari <35136890+poojakumari11228@users.noreply.github.com>
2020-08-13 03:14:18 +0500

    create & delete candidates

--- assets/script/image-holder.js
+++ assets/script/image-holder.js
@@ -11,3 +11,20 @@ function readURL(input) {
 $("#profile-pic").change(function () {
     readURL(this);
 });
+
+// For manage candidates
+function readCandidateURL(elem, tagId) 
+{
+      if (elem.files && elem.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $(tagId).attr('src', e.target.result);
+        }
+        reader.readAsDataURL(elem.files[0]);
+    }
+}
+
+// for create candidate
+$("#create-candidate-pic").change(function () {
+    readCandidateURL(this, "#create-candidate-profile-imgTag");
+});
