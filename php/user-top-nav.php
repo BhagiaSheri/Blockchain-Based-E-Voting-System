cc9eb55c6d97bb4604895e00ec00662446c23a46
BhagiaSheri <bhagiasheri24@gmail.com>
2020-08-23 02:35:36 +0500

    vote cast - user frontend

---
+++ php/user-top-nav.php
@@ -0,0 +1,17 @@
+<nav class="sb-topnav navbar navbar-expand" id="navbar" style="background-color: #b3aeab;">
+    <a class="navbar-brand" href="#" style="margin:0px;"><img src="../assets/images/logo-vo-blok.png" width="150" style="margin:0px;"></a>
+    <!-- Navbar-->
+    <ul class="navbar-nav ml-auto mr-0 mr-md-3 my-2 my-md-0">
+
+        <!-- for logged in user in navbar -->
+        <li class="nav-item dropdown" >
+            <a class="nav-link dropdown-toggle text-white" id="userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+                <a class="dropdown-item" href="#">User</a>
+                <a class="dropdown-item" href="#" id="editProfile">Settings</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" id="log-me-out" href="#" >Logout</a>
+            </div>
+        </li>
+    </ul>
+</nav>
---
+++ php/user.php
@@ -0,0 +1,261 @@
+<?php
+include_once("../header.html");
+?>
+<title>Cast Vote</title>
+<!-- add icon link -->
+<link rel="icon" href="../assets/images/logo.png" type="image/x-icon">
+</head>
+
+<?php
+include_once("user-top-nav.php");
+?>
+<section id="candidates-list">
+	<!-- President List -->
+	<div class="container-fluid">
+		<h1 class="py-4">Candidates</h1>
+		<h5 class="subtitle">Presidents List</h5>
+
+		<div class="scrolling-wrapper row flex-row flex-nowrap mt-4 pb-4">
+
+			<div class="col-5">
+				<div class="card card-block">
+					<div class="row no-gutters">
+						<div class="col-sm-5">
+							<img class="card-img" src="../assets/images/user.png" alt="Suresh Dasari Card">
+						</div>
+						<div class="col-sm-7 d-flex flex-column justify-content-center">
+							<div class="card-body d-flex flex-column justify-content-center">
+								<h4 class="card-title">Suresh Dasari</h4>
+								<p class="card-text">President</p>
+								<a href="#" class="btn btn-success">Cast Vote</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-5">
+				<div class="card card-block">
+					<div class="row no-gutters">
+						<div class="col-sm-5">
+							<img class="card-img" src="../assets/images/user.png" alt="Suresh Dasari Card">
+						</div>
+						<div class="col-sm-7 d-flex flex-column justify-content-center">
+							<div class="card-body d-flex flex-column justify-content-center">
+								<h4 class="card-title">Suresh Dasari</h4>
+								<p class="card-text">President</p>
+								<a href="#" class="btn btn-success">Cast Vote</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-5">
+				<div class="card card-block">
+					<div class="row no-gutters">
+						<div class="col-sm-5">
+							<img class="card-img" src="../assets/images/user.png" alt="Suresh Dasari Card">
+						</div>
+						<div class="col-sm-7 d-flex flex-column justify-content-center">
+							<div class="card-body d-flex flex-column justify-content-center">
+								<h4 class="card-title">Suresh Dasari</h4>
+								<p class="card-text">President</p>
+								<a href="#" class="btn btn-success">Cast Vote</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-5">
+				<div class="card card-block">
+					<div class="row no-gutters">
+						<div class="col-sm-5">
+							<img class="card-img" src="../assets/images/user.png" alt="Suresh Dasari Card">
+						</div>
+						<div class="col-sm-7 d-flex flex-column justify-content-center">
+							<div class="card-body d-flex flex-column justify-content-center">
+								<h4 class="card-title">Suresh Dasari</h4>
+								<p class="card-text">President</p>
+								<a href="#" class="btn btn-success">Cast Vote</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-5">
+				<div class="card card-block">
+					<div class="row no-gutters">
+						<div class="col-sm-5">
+							<img class="card-img" src="../assets/images/user.png" alt="Suresh Dasari Card">
+						</div>
+						<div class="col-sm-7 d-flex flex-column justify-content-center">
+							<div class="card-body d-flex flex-column justify-content-center">
+								<h4 class="card-title">Suresh Dasari</h4>
+								<p class="card-text">President</p>
+								<a href="#" class="btn btn-success">Cast Vote</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+	<!-- General Secatry List -->
+	<div class="container-fluid">
+		<h5 class="subtitle mt-4">General Secatary List</h5>
+
+		<div class="scrolling-wrapper row flex-row flex-nowrap mt-4 pb-4">
+
+			<div class="col-5">
+				<div class="card card-block">
+					<div class="row no-gutters">
+						<div class="col-sm-5">
+							<img class="card-img" src="../assets/images/user.png" alt="Suresh Dasari Card">
+						</div>
+						<div class="col-sm-7 d-flex flex-column justify-content-center">
+							<div class="card-body d-flex flex-column justify-content-center">
+								<h4 class="card-title">Suresh Dasari</h4>
+								<p class="card-text">General Secatry</p>
+								<a href="#" class="btn btn-success">Cast Vote</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-5">
+				<div class="card card-block">
+					<div class="row no-gutters">
+						<div class="col-sm-5">
+							<img class="card-img" src="../assets/images/user.png" alt="Suresh Dasari Card">
+						</div>
+						<div class="col-sm-7 d-flex flex-column justify-content-center">
+							<div class="card-body d-flex flex-column justify-content-center">
+								<h4 class="card-title">Suresh Dasari</h4>
+								<p class="card-text">General Secatry</p>
+								<a href="#" class="btn btn-success">Cast Vote</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-5">
+				<div class="card card-block">
+					<div class="row no-gutters">
+						<div class="col-sm-5">
+							<img class="card-img" src="../assets/images/user.png" alt="Suresh Dasari Card">
+						</div>
+						<div class="col-sm-7 d-flex flex-column justify-content-center">
+							<div class="card-body d-flex flex-column justify-content-center">
+								<h4 class="card-title">Suresh Dasari</h4>
+								<p class="card-text">General Secatry</p>
+								<a href="#" class="btn btn-success">Cast Vote</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-5">
+				<div class="card card-block">
+					<div class="row no-gutters">
+						<div class="col-sm-5">
+							<img class="card-img" src="../assets/images/user.png" alt="Suresh Dasari Card">
+						</div>
+						<div class="col-sm-7 d-flex flex-column justify-content-center">
+							<div class="card-body d-flex flex-column justify-content-center">
+								<h4 class="card-title">Suresh Dasari</h4>
+								<p class="card-text">General Secatry</p>
+								<a href="#" class="btn btn-success">Cast Vote</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+	<!-- Director General List -->
+	<div class="container-fluid">
+		<h5 class="subtitle mt-4">Director General List</h5>
+
+		<div class="scrolling-wrapper row flex-row flex-nowrap mt-4 pb-4">
+
+			<div class="col-5">
+				<div class="card card-block">
+					<div class="row no-gutters">
+						<div class="col-sm-5">
+							<img class="card-img" src="../assets/images/user.png" alt="Suresh Dasari Card">
+						</div>
+						<div class="col-sm-7 d-flex flex-column justify-content-center">
+							<div class="card-body d-flex flex-column justify-content-center">
+								<h4 class="card-title">Suresh Dasari</h4>
+								<p class="card-text">Director General</p>
+								<a href="#" class="btn btn-success">Cast Vote</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-5">
+				<div class="card card-block">
+					<div class="row no-gutters">
+						<div class="col-sm-5">
+							<img class="card-img" src="../assets/images/user.png" alt="Suresh Dasari Card">
+						</div>
+						<div class="col-sm-7 d-flex flex-column justify-content-center">
+							<div class="card-body d-flex flex-column justify-content-center">
+								<h4 class="card-title">Suresh Dasari</h4>
+								<p class="card-text">Director General</p>
+								<a href="#" class="btn btn-success">Cast Vote</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-5">
+				<div class="card card-block">
+					<div class="row no-gutters">
+						<div class="col-sm-5">
+							<img class="card-img" src="../assets/images/user.png" alt="Suresh Dasari Card">
+						</div>
+						<div class="col-sm-7 d-flex flex-column justify-content-center">
+							<div class="card-body d-flex flex-column justify-content-center">
+								<h4 class="card-title">Suresh Dasari</h4>
+								<p class="card-text">Director General</p>
+								<a href="#" class="btn btn-success">Cast Vote</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-5">
+				<div class="card card-block">
+					<div class="row no-gutters">
+						<div class="col-sm-5">
+							<img class="card-img" src="../assets/images/user.png" alt="Suresh Dasari Card">
+						</div>
+						<div class="col-sm-7 d-flex flex-column justify-content-center">
+							<div class="card-body d-flex flex-column justify-content-center">
+								<h4 class="card-title">Suresh Dasari</h4>
+								<p class="card-text">Director General</p>
+								<a href="#" class="btn btn-success">Cast Vote</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</section>
