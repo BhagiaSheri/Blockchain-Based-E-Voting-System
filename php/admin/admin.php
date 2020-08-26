ee9dfdcb06b5b5bd686ecef18c5fe7a7c7573dc4
BhagiaSheri <bhagiasheri24@gmail.com>
2020-08-26 06:28:41 +0500

    charts on admin & user page from smart contract data

--- php/admin/admin.php
+++ php/admin/admin.php
@@ -1,7 +1,9 @@
 <?php
 include_once("header.html");
 ?>
+
 <title>Admin</title>
+
 <link rel="stylesheet" href="../../assets/lib/bootstrap-4.5.2/css/bootstrap.min.css">
 
 <link href="../../assets/css/admin.css" rel="stylesheet" />
@@ -27,31 +29,14 @@ include_once("header.html");
     <div id="layoutSidenav_content">
         <main>
             <div class="container-fluid">
-                <h1 class="mt-4">Results</h1>
-                <!-- reults charts -->
-                <div class="row">
-                    <div class="col-xl-6">
-                        <div class="card mb-4">
-                            <div class="card-header">
-                                <i class="fa fa-pie-chart mr-1"></i>
-                                Pie Chart
-                            </div>
-                            <div class="card-body"><canvas id="myPieChart" width="100%" height="40"></canvas></div>
-                        </div>
-                    </div>
-                    <div class="col-xl-6">
-                        <div class="card mb-4">
-                            <div class="card-header">
-                                <i class="fas fa-chart-bar mr-1"></i>
-                                Bar Chart
-                            </div>
-                            <div class="card-body"><canvas id="myBarChart" width="100%" height="40"></canvas></div>
-                        </div>
-                    </div>
-                </div>
 
-                <!-- Connection to Database -->
                 <?php
+                // results charts html file
+                include_once("../../html/charts.html");
+                // bar and pic chart file
+                include_once("../result-charts/charts.php");
+
+                // Connection to Database 
                 global $conn;
                 if ($conn == null) {
                     include_once("../config/connection.php");
@@ -133,9 +118,9 @@ include_once("header.html");
         </main>
     </div>
 
-    <?php
-    include_once("footer-script.html");
-    ?>
 </body>
+<?php
+include_once("footer-script.html");
+?>
 
 </html>
--- php/user-top-nav.php
+++ php/user-top-nav.php
@@ -1,23 +1,26 @@
 <?php
 session_start();
-if (!(isset($_SESSION['role']) ) && $_SESSION['role'] != 'user') {
+if (!(isset($_SESSION['role'])) && $_SESSION['role'] != 'user') {
     header("location:../../index.php");
-}else
-$name = $_SESSION["user_name"] ;
+} else
+    $name = $_SESSION["user_name"];
 ?>
 <nav class="sb-topnav navbar navbar-expand" id="navbar" style="background-color: #b3aeab;">
     <a class="navbar-brand" href="#" style="margin:0px;"><img src="../assets/images/logo-vo-blok.png" width="150" style="margin:0px;"></a>
     <!-- Navbar-->
     <ul class="navbar-nav ml-auto mr-0 mr-md-3 my-2 my-md-0">
+        <li class="nav-item">
+            <a href="election-statistics.php" class="btn btn-primary"><i class="fa fa-bar-chart" aria-hidden="true"></i> Election Statistics</a>
+        </li>
 
         <!-- for logged in user in navbar -->
-        <li class="nav-item dropdown" >
+        <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle text-white" id="userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
                 <a class="dropdown-item" href="#"><?php echo $name ?></a>
                 <a class="dropdown-item user" href="#" id="editProfile">Settings</a>
                 <div class="dropdown-divider"></div>
-                <a class="dropdown-item user" id="log-me-out" href="#" >Logout</a>
+                <a class="dropdown-item user" id="log-me-out" href="#">Logout</a>
             </div>
         </li>
     </ul>
